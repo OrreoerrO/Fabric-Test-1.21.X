b52425297eddc5c68a4bde3dda80ccf784e5e56e
orrin <orrin@kora.net.au>
2026-02-23 21:39:05 +1100

    Added Super Duper Important Comment

--- src/main/java/com/orrinmcgorrin/TestModClient.java
+++ src/main/java/com/orrinmcgorrin/TestModClient.java
@@ -2,6 +2,8 @@ package com.orrinmcgorrin;
 
 import net.fabricmc.api.ClientModInitializer;
 
+// Super Duper Important Comment That Is Crazy Important
+
 public class TestModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
